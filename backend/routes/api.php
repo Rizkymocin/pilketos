b088ada2d9368724eb0924aebf27bc15f01f35e5
Rizkymocin <rizky.mohi30@gmail.com>
2025-10-03 21:55:14 +0800

    Login Mechanisme done

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware("auth:sanctum")->group(function() {
     Route::post('logout', [AuthController::class, 'logout']);
